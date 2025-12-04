d83db26ce0d33fd214222baa8fbde984d270aa58
husamettinarabaci <harabaci88@gmail.com>
2025-12-04 23:20:55 +0300

    feat(setup): first initialize
    First repo created finished

--- widgetbook_kit/lib/app/widgetbook_app.dart
+++ widgetbook_kit/lib/app/widgetbook_app.dart
@@ -6,6 +6,7 @@ import 'package:widgetbook/widgetbook.dart';
 import 'package:widgetbook_annotation/widgetbook_annotation.dart' as widgetbook;
 
 import 'widgetbook_app.directories.g.dart';
+import 'package:core_kit/core_kit.dart';
 
 @widgetbook.App()
 class HexaWidgetbookApp extends StatelessWidget {
@@ -18,14 +19,8 @@ class HexaWidgetbookApp extends StatelessWidget {
       addons: [
         MaterialThemeAddon(
           themes: [
-            WidgetbookTheme(
-              name: 'Light',
-              data: ThemeData(brightness: Brightness.light, useMaterial3: true),
-            ),
-            WidgetbookTheme(
-              name: 'Dark',
-              data: ThemeData(brightness: Brightness.dark, useMaterial3: true),
-            ),
+            WidgetbookTheme(name: 'Light', data: AppTheme.light()),
+            WidgetbookTheme(name: 'Dark', data: AppTheme.dark()),
           ],
         ),
       ],
